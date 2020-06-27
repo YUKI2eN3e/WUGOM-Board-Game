1474dc108dfc8db04aba44e2d402a29d8c46b52a
MikuHatsune39 <32179075+MikuHatsune39@users.noreply.github.com>
2020-06-27 16:40:53 -0700

    Completed board

--- game.py
+++ game.py
@@ -8,10 +8,29 @@ class Space:
         return self.value
     
 
-board = [Space("Start",0),
+board = [Space("START!",0),
     Space("At any rate, life is hard -150 points",-150),
     Space("Naturally attractive +200 points",200),
-    Space("Deodorant spray ran out -50 points",-50)]
+    Space("Deodorant spray ran out -50 points",-50),
+    Space("Hayasaka cleans the room +100 points",100),
+    Space("The clothes you bought don't suit you -80 points",-80),
+    Space("Recive a rice cracker from Igarashi-san +100",100),
+    Space("The toilet broke -120 points",-120),
+    Space("Go back to sleep +200 points",-200),
+    Space("Orie praises your voice +100 points",100),
+    Space("Save money for food +80 points",80),
+    Space("Pay for Yasuda's living expenses -500 points",-500),
+    Space("You've been doxed on social media -140 points",-140),
+    Space("You get woken up on a day off -200 points",-200),
+    Space("Several hot days in a row -70 points",-70),
+    Space("Sugimura pays for you +100 points",100),
+    Space("Everyone respects you in class +120 points",120),
+    Space("Sucking up tapioca balls is so tiring -50 points",-50),
+    Space("You're victorious in a ball sports tournament +200 points",200),
+    Space("A manga you borrowed was interesting +80 points",80),
+    Space("Work -250 points",-250),
+    Space("You wake up as a girl one morning ±0 points",0),
+    Space("GOAL!",0)]
 
 for space in board:
     print(space.getText() + " : " + str(space.getValue()))
